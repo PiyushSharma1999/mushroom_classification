6a0e1dae3252583c218ad21a066038f976c20873
Piyush Sharma <piyush316692@gmail.com>
2022-09-20 17:29:37 +0530

    Added Prediction_Raw_Data_Validation module

---
+++ Prediction_Raw_Data_Validation/predictionDataValidation.py
@@ -0,0 +1,254 @@
+import sqlite3
+from datetime import datetime
+from os import listdir
+import os
+import re
+import json
+import shutil
+import pandas as pd
+from application_logging.logger import App_logger
+
+
+class Prediction_Data_validation:
+    """
+        This class shall be used for handling all the validation done on the Raw Prediction Data!!. 
+    """
+    def __init__(self,path):
+        self.Batch_directory = path
+        self.schema_path = 'schema_prediction.json'
+        self.logger = App_logger()
+
+    def valuesFromSchema(self):
+        """
+            Method Name: valuesFromSchema
+            Description: This method extracts all the relevant information fromthe pre-defines schema file.
+            Output: LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, Number of columns
+            On Failure: Raise ValueError, KeyError, Exception
+        """
+        try:
+            with open(self.schema_path,'r' ) as f : 
+                dic = json.load(f)
+                f.close()
+                patter = dic['SampleFileName']
+                LengthOfDateStampInFile = dic['LengthOfDateStampInFile']
+                LengthOfTimeStampInFile=dic['LengthOfTimeStampInFile']
+                column_names = dic['ColName']
+                NumberofColumns = dic['NumberofColumns']
+
+                file = open("Training_Logs/valuesfromSchemaValidation.txt",'a+')
+                message = "LengthOfDateStampInFile:: %s"%LengthOfDateStampInFile+"\t"+"LengthOfTimeStampInFile:: %s"%LengthOfTimeStampInFile+"\t"+"NumberofColumns:: %s"%NumberofColumns+"\n"
+                self.logger.log(file,message)
+
+                file.close()
+
+        except ValueError:
+            file = open("Prediction_Logs/valuesfromSchemaValidationLog.txt",'a+')
+            self.logger.log(file,"ValueError: Value not found inside schema_training.json")
+            file.close()
+            raise ValueError
+        
+        except KeyError:
+            file = open("Prediction_Logs/valuesfromSchemaValidation.txt",'a+')
+            self.logger.log(file,"KeyError: Key value error incorrect key passed")
+            file.close()
+            raise KeyError
+        
+        except Exception as e:
+            file = open("Prediction_Logs/valuesfromSchemaValidation.txt",'a+')
+            self.logger.log(file,str(e))
+            file.close()
+            raise e
+
+        return LengthOfDateStampInFile,LengthOfTimeStampInFile,column_names,NumberofColumns
+    
+    def manualRegexCreation(self):
+        """
+            Method Name: maualRegexCreateion
+            Description: This method contains a manualyy defined regex base on the "FileName" given in "Schema" file.
+            Output : Regex Pattern
+            On Failure: None
+        """
+        regex  = "['mushroom']+['\_'']+[\d_]+\.csv"
+        return regex
+    
+    def createDirectoryForGoodBadRawData(self):
+        """
+            Method Name: createDirectoryForGoodBadRawData
+            Description: This  method creates directories to store the Good Data and Bad Data
+            Output: None
+            On Failure: OSError
+        """
+        try:
+            path  = os.path.join("Prediction_Raw_Files_Validated/","Good_Raw/")
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            path = os.path.join("Prediction_Raw_Files_Validated/","Bad_Raw/")
+            if not os.path.isdir(path):
+                os.makedirs(path)
+        
+        except OSError as ex:
+            file = open("Prediction_Logs/GeneralLog.txt",'a+')
+            self.logger.log(file,"Error while creating Directory :%s"%ex)
+            file.close()
+            raise OSError
+        
+    def deleteExistingGoodDataTrainingFolder(self):
+        """
+            Method Name: deleteExistingGoodDataTrainingFolder
+            Description: This method deletes the directory made to store the Good Data
+                         after loading the data in the table. Once the good files are
+                         loaded in the DB, deleting the directory ensures space optimization.
+            Output: None
+            On Failure: OSError
+        """
+        try:
+            path = 'Prediction_Raw_Files_Validated'
+            if os.path.isdir(path + 'Good_Raw/'):
+                shutil.rmtree(path+'Good_Raw')
+                file = open("Prediction_Logs/GeneralLog.txt",'a+')
+                self.logger.log(file,"GoodRaw directory deleted succcessfully!!")
+                file.close()
+        except OSError as s:
+            file = open("Prediction_Logs/GeneralLog.txt",'a+')
+            self.logger.log(file,"Error while Deleting Directory: %s"%s)
+            file.close()
+            raise OSError 
+    
+    def deleteExistingBadDataTrainingFolder(self):
+        """
+            Method Name: deleteExistingBadDataTrainingFolder
+            Description: This method deletes the directory made to store the bad data.
+            Output: None
+            On Failure: OSError
+        """
+        try:
+            path = 'Rediction_Raw_Files_Validated'
+            if os.path.isdir(path+"Bad_Raw/"):
+                shutil.rmtree(path+"Bad_Raw/")
+                file = open("Prediction_Logs/GeneralLog.txt",'a+')
+                self.logger.log(file,"BadRaw directory deleted before starting validation!!")
+                file.close()
+        except OSError as s:
+            file = open("Prediction_Logs/GeneralLog.txt",'a+')
+            self.logger.log(file,"Error while Deleting Directory: %s"%s)
+            file.close()
+            raise OSError
+    
+    def moveBadFilesToArchiveBad(self):
+        """
+            Method Name: moveBadFilesToArchiveBad
+            Description: This method deletes the directory made to store the Bad data
+                         after moving the data in an archive folde. e archive the bad 
+                         file to send them back to the client for invalid data issue.
+            Output: None
+            On Failure: OSError
+        """
+        now = datetime.now()
+        date = now.date()
+        time = now.strftime("%H%M%S")
+        try:
+            path = "PredictionArchiveBadData"
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            source = 'Prediction_Raw_Files_Validated/Bad_Raw/'
+            dest = 'PredictionArchiveBadData/BadData_'+str(date)+"_"+str(time)
+            if not os.path.isdir(dest):
+                os.makedirs(dest)
+            files = os.listdir(source)
+            for f in files:
+                if f not in os.listdir(dest):
+                    shutil.move(source+f,dest)
+            file = open("Prediction_Logs/GeneralLig.txt",'a+')
+            self.logger.log(file,"Bad files moved  to archive")
+            path = 'Prediction_Raw_Files_Validated'
+            if os.path.isdir(path+'Bad_Raw/'):
+                shutil.rmtree(path+'Bad_Raw/')
+            self.logger.log(file,"Bad Raw Data Folde Deleted Successfully!!")
+            file.close()
+        except OSError as e:
+            file = open("Prediction_Logs/GeneralLog.txt",'a+')
+            self.logger.log(file,"Error while moving bad files to archive:: %s"%e)
+            file.close()
+            raise OSError
+    
+    def valdiationFileNameRaw(self,regex,LengthOfDateStampInFile,LengthOfTimeStampInFile):
+        """
+            Method Name: valdiationFileNameRaw
+            Description: This fuction validated the name of the prediction csv file as per given name in the schema.
+                         Regex patter is used to dothe validation. If name fo not match the file is moved to Bad 
+                         Raw Data folder or else in Good Raw Data Folder.
+            Output: None
+            On Failure: Raise Exception
+        """
+        # delete the directories for good and bad data in case last run was unsuccessful and folder were not deleted.
+        self.deleteExistingBadDataTrainingFolder()
+        self.deleteExistingGoodDataTrainingFolder()
+        self.createDirectoryForGoodBadRawData()
+        onlyfiles = [f for f in listdir(self.Batch_directory)]
+        try:
+            f = open("Prediction_Logs/nameValidationLog.txt",'a+')
+            for filename in onlyfiles:
+                if (re.match(regex,filename)):
+                    splitAtDot = re.split('.csv',filename)
+                    splitAtDot = (re.split('_',splitAtDot[0]))
+                    if len(splitAtDot[1])==LengthOfDateStampInFile:
+                        if len(splitAtDot[2])==LengthOfTimeStampInFile:
+                            shutil.copy("Prediction_Batch_files/"+filename,"Prediction_Raw_Files_Validated/Good_Raw")
+                            self.logger.log(f,"Valid File Name!! File moved to Good Raw Flder:: %s"%filename)
+                        else:
+                            shutil.copy("Prediction_Batch_files/"+filename,"Prediction_Raw_Files_Validated/Bad_Raw")
+                            self.logger.log(f,"Invalid File Name!! File moved to Bad Raw Folder:: %s"%filename)
+                    else:
+                        shutil.copy("Prediction_Batch_files/"+filename,"Prediction_Raw_Files_Validated/Bad_Raw")
+                        self.logger.log(f,"Invalid File Name!! File moved to Bad Raw Folder:: %s"%filename)
+                else:
+                    shutil.copy("Prediction_Batch_files/"+filename,"Prediction_Raw_Files_Validated/Bad_Raw")
+                    self.logger.log(f,"Invalid File Name!! File moved to Bad Raw Folder:: %s"%filename)
+            f.close()
+        
+        except Exception as e:
+            f = open("Prediction_Logs/nameValidationLog.txt",'a+')
+            self.logger.log(f,"Error occured while validating FileName %s"%e)
+            f.close()
+            raise e
+
+    def validateColumnLength(self,NumberofColumns):
+        """
+            Method Name: validateColumnLength
+            Description: This function validates the number of columns in the csv files.
+                         It should be same as given in the schema file.
+                         If not same file is not suitable for processing and thus is moved to Bad Raw folder.
+                         If the column number matches, file is kept in Good Raw Data for processing.
+                         The csv file is missing the first column name, this function changes the missing name to "Mushroom".
+            Output: None
+            On Failure: Raise Exception
+        """
+        try:
+            f = open("Prediction_Logs/columnValidationLog.txt",'a+')
+            self.logger.log(f,"Column Length validation Started!!")
+            for file in listdir('Prediction_Raw_Files_Validated/Good_Raw/'):
+                csv = pd.read_csv("Prediction_Raw_Files_Validated/Good_Raw/"+file)
+                if csv.shape[1]==NumberofColumns:
+                    csv.rename(columns={"Unnamed:0":"Mushroom"},inplace=True)
+                    csv.to_csv("Prediction_Raw_Files_Validated/Good_Raw/"+file,index=None,header=True)
+                else:
+                    shutil.move("Prediction_Raw_Files_Validated/Good_Raw/"+file,"Prediction_Raw_Files_Validated/Bad_Raw")
+                    self.logger.log(f,"Invalid Column Length for the file!! File moved to Bad Raw Folder:: %s"%file)
+            self.logger.log(f,"Column Length Validation Completed!!")
+        except OSError:
+            f = open("Prediction_Logs/columnValidationLog.txt",'a+')
+            self.logger.log(f,"Error Occured while moving the file :: %s"%OSError)
+            f.close()
+            raise OSError
+        except Exception as e:
+            f = open("Prediction_Logs/columnValidationLog.txt",'a+')
+            self.logger.log(f,"Error Occured:: %s"%e)
+            f.close()
+            raise e
+
+        f.close()
+    
+    def deletedPredictionFile(self):
+
+        if os.path.exists('Prediction_Output_File/Predictions.csv'):
+            os.remove('Prediction_Output_File/Predictions.csv')
