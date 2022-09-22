2c983a3fb7d884c781540064788fc16949e8b460
Piyush Sharma <piyush316692@gmail.com>
2022-09-22 13:53:41 +0530

    Added clustering module to data_preprocessing package, Added file_operations package

---
+++ file_operations/file_methods.py
@@ -0,0 +1,9 @@
+import pickle 
+import os
+import shutil
+
+class File_Operations:
+    """
+        This class shall be used to save the model after training
+        and load the saved model for prediction.
+    """
