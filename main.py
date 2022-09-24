5230902dbd40ab0bd46285c1960b1e1657f72a7b
Piyush Sharma <piyush316692@gmail.com>
2022-09-24 14:58:38 +0530

    Added prediction function to Prediction route in main.py

--- main.py
+++ main.py
@@ -21,6 +21,42 @@ CORS(app)
 def home():
     return render_template('index.html')
 
+@app.route("/", methods=['GET'])
+@cross_origin
+def predictRouteClient():
+    try:
+        if request.json is not None:
+            path = request.json['filepath']
+
+            pred_val = pred_validation(path) # object initialization
+
+            pred_val.prediction_validation() # calling the prediction_validation function
+
+            pred = prediction(path) # object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s"%path)
+        elif request.form is not None:
+            path = request.form['filepath']
+
+            pred_val = pred_validation(path) # object initialization
+
+            pred_val.prediction_validation() # calling the prediction_validation function
+
+            pred = prediction(path) # object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s!!"%path)
+    
+    except ValueError:
+        return Response("Error Occured! %s"%ValueError)
+    except KeyError:
+        return Response("Error Occured! %s"%KeyError)
+    except Exception as e:
+        return Response("Error  Occured! %s"%e)
+
 
 
 if __name__ == '__main__':
